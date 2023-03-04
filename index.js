113142506dff97d6d09dfc946eef42af41f1fea8
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-03-04 00:38:39 -0800

    Adds myfile

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(arrayElement, eventName){
+    const arrName = [];    
+    for (let i = 0; i < arrayElement.length; i++){
+            let message = (`Thank you, ${arrayElement[i]}, for the wonderful ${eventName} gift!`);
+      
+        arrName.push(message);           
+        }
+return arrName;
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+function countDown(number){
+
+while (number >= 0){
+console.log(number--);}
+return
+}
+countDown (10);
